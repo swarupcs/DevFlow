2a494080709e96044cd770c64884508aa44bdfc0
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-10-26 03:51:14 +0530

    Implement Create Question Form Logic

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,16 +1,23 @@
-import QuestionForm from "@/components/forms/QuestionForm";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const AskAQuestion = () => {
-   return (
-     <>
-       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+import { auth } from "@/auth";
+import QuestionForm from "@/components/forms/QuestionForm";
 
-       <div className="mt-9">
-         <QuestionForm />
-       </div>
-     </>
-   );
+const AskQuestion = async () => {
+  const session = await auth();
+
+  if (!session) return redirect("/sign-in");
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+      <div className="mt-9">
+        <QuestionForm />
+      </div>
+    </>
+  );
 };
 
-export default AskAQuestion;
+export default AskQuestion;
